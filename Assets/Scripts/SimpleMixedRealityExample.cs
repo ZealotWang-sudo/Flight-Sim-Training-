d4e6c6e36a7cc035e8d8f1c87144e582bf993acf
Kexin Wang <kewa2435@win.dsv.su.se>
2022-05-10 14:52:14 +0200

    added all the functions of mixed reality

---
+++ Assets/Scripts/SimpleMixedRealityExample.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Varjo.XR;
+
+public class SimpleMixedRealityExample : MonoBehaviour
+{
+    public bool mixedReality;
+    public KeyCode MixedRealityToggle = KeyCode.Space;
+
+    private bool mixedRealityEnabled = false;
+    private bool originalOpaqueValue;
+
+    void Start()
+    {
+        if (mixedReality)
+        {
+            VarjoMixedReality.StartRender(); 
+            VarjoMixedReality.EnableDepthEstimation();
+            mixedRealityEnabled = true;
+            originalOpaqueValue = VarjoRendering.GetOpaque();
+            VarjoRendering.SetOpaque(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(MixedRealityToggle))
+        {
+            mixedReality = !mixedReality;
+        }
+
+        if (mixedReality != mixedRealityEnabled)
+        {
+            if (mixedReality)
+            {
+                VarjoMixedReality.StartRender();
+                VarjoMixedReality.EnableDepthEstimation();
+                originalOpaqueValue = VarjoRendering.GetOpaque();
+                VarjoRendering.SetOpaque(false);
+            }
+            else
+            {
+                VarjoMixedReality.DisableDepthEstimation();
+                VarjoMixedReality.StopRender();
+             
+                VarjoRendering.SetOpaque(originalOpaqueValue);
+            }
+
+            mixedRealityEnabled = mixedReality;
+        }
+    }
+}
